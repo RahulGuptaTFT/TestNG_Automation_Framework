6a01d64658e21a842ed1be6a9efe7d557c9d71b8
RahulGuptaTFT <g.rahul@tftus.com>
2023-11-02 16:32:12 +0530

    2 commit

--- src/main/java/project/basetest/BaseTest.java
+++ src/main/java/project/basetest/BaseTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
-import project.Utils.Listeners;
-
 import java.time.Duration;
 
 
